2bee6793019500951fd89a703454f590dc364b71
master <1837110636@qq.com>
2018-04-18 19:24:48 +0800

    1

--- application/index/controller/Friend.php
+++ application/index/controller/Friend.php
@@ -14,7 +14,26 @@ class Friend extends Controller{
 
         return $this->fetch();
     }
-    public function data(){
+    public function loadFriend(){
+        $userId=Session::get('uinfo')['userId'];
+        $list=db('friend')->where('userId1',$userId)->whereOr('userId2',$userId);
+    }
+    public function addFriend(){
+        $userId1=Session::get('uinfo')['userId'];
+        $userId2=$_POST('userid');
+        $createtime=date(date('Y-m-d H:i:s'));
+        $list=db('friend')->where(['userId1'=>$userId1,'userId2'=>$userId2])->whereOr(['userId1'=>$userId2,'userId2'=>$userId1])->select();
+        if($list){
+            db('friend')->insert(['userId1'=>$userId1,'userId2'=>$userId2,'createtime'=>$createtime]);
+        }
+    }
+    public function deleteFriend(){
+        
+    }
+    public function sendMessage(){
+
+    }
+    public function loadMessage(){
 
     }
 }
