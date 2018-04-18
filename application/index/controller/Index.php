91812f8ac6a41a9307a43b0c618a90fcb978f13d
master <1837110636@qq.com>
2018-04-18 15:59:39 +0800

    1

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,7 +1,9 @@
 <?php
 namespace app\index\controller;
 use think\Controller;
+use think\Env;
 use think\Session;
+
 
 class Index extends Controller
 {
@@ -37,59 +39,58 @@ class Index extends Controller
            }
            else{
                 $this->error('文件名已存在');
-
            }
-
-
        }
 
    }
 
     public function upload(){           //上传文件
-
-        if ((@$_FILES["file"]["type"] == "image/gif") || (@$_FILES["file"]["type"] == "image/jpeg")
-            || (@$_FILES["file"]["type"] == "image/peg") || (@$_FILES["file"]["type"] == "image/png")
-            &&(@$_FILES["file"]["size"] <500 * 1024 * 1024)){
-            $files['filetype']=1;
-
-            
-            function createFolder($path){
-                if (!file_exists($path)) {
-                    createFolder(dirname($path));
-                    mkdir($path, 0777);
-                }
-            }
-
-            createFolder("upload/");
-            move_uploaded_file($_FILES["file"]["tmp_name"],"upload/i.jpg");
-        }
-        elseif((@$_FILES["file"]["type"] == "video/mpeg") || (@$_FILES["file"]["type"] == "video/quicktime")
-            || (@$_FILES["file"]["type"] == "video/x-la-asf") || (@$_FILES["file"]["type"] == "video/x-ms-asf")
-            || (@$_FILES["file"]["type"] == "video/x-msvideo")||(@$_FILES["file"]["type"] == "video/x-sgi-movie")
-            &&(@$_FILES["file"]["size"] <500 * 1024 * 1024)){
-            $files['filetype']=4;
-        }
-        elseif((@$_FILES["file"]["type"] == "text/plain") || (@$_FILES["file"]["type"] == "application/msword")
-            || (@$_FILES["file"]["type"] == "application/vnd.ms-powerpoint") || (@$_FILES["file"]["type"] == "application/vnd.ms-excel")
-            || (@$_FILES["file"]["type"] == "x-world/x-vrml")|| (@$_FILES["file"]["type"] == "text/html")
-            &&(@$_FILES["file"]["size"] <500 * 1024 * 1024)){
-            $files['filetype']=2;
-        }
-        elseif ((@$_FILES["file"]["type"] == "audio/mpeg") || (@$_FILES["file"]["type"] == "audio/x-aiff")
-            || (@$_FILES["file"]["type"] == "audio/x-wav") || (@$_FILES["file"]["type"] == "audio/x-pn-realaudio")
-            &&(@$_FILES["file"]["size"] <500 * 1024 * 1024)){
-            $files['filetype']=3;
-        }
-        else{
-            $files['filetype']=5;
-        }
-
-
         $files['filename']=@$_FILES["file"]["name"];
         $files['filesize']=@$_FILES["file"]["size"];
         $files['useId']=Session::get('uinfo')['userId'];
         $files['folderid']=$_POST('parentid');
         $files['createtime']=date(date('Y-m-d H:i:s'));
+        function createFolder($path){
+            if (!file_exists($path)) {
+                createFolder(dirname($path));
+                mkdir($path, 0777);
+            }
+        }
+        if ((@$_FILES["file"]["type"] == "image/gif") || (@$_FILES["file"]["type"] == "image/jpeg")
+            || (@$_FILES["file"]["type"] == "image/peg") || (@$_FILES["file"]["type"] == "image/png")
+            &&(@$_FILES["file"]["size"] <500 * 1024 * 1024)){
+            $files['filetype']=1;
+            createFolder("upload/".$files['userId']."/image/");
+            move_uploaded_file($_FILES["file"]["tmp_name"],"upload/i.jpg");
+        }
+        elseif((@$_FILES["file"]["type"] == "video/mpeg") || (@$_FILES["file"]["type"] == "video/quicktime")            //判断是视频类型
+            || (@$_FILES["file"]["type"] == "video/x-la-asf") || (@$_FILES["file"]["type"] == "video/x-ms-asf")
+            || (@$_FILES["file"]["type"] == "video/x-msvideo")||(@$_FILES["file"]["type"] == "video/x-sgi-movie")
+            &&(@$_FILES["file"]["size"] <500 * 1024 * 1024)){
+            $files['filetype']=4;
+            createFolder("upload/".$files['userId']."/video/");
+        }
+        elseif((@$_FILES["file"]["type"] == "text/plain") || (@$_FILES["file"]["type"] == "application/msword")         //判断是文档类型
+            || (@$_FILES["file"]["type"] == "application/vnd.ms-powerpoint") || (@$_FILES["file"]["type"] == "application/vnd.ms-excel")
+            || (@$_FILES["file"]["type"] == "x-world/x-vrml")|| (@$_FILES["file"]["type"] == "text/html")
+            &&(@$_FILES["file"]["size"] <500 * 1024 * 1024)){
+            $files['filetype']=2;
+            createFolder("upload/".$files['userId']."/document/");
+        }
+        elseif ((@$_FILES["file"]["type"] == "audio/mpeg") || (@$_FILES["file"]["type"] == "audio/x-aiff")              //判断是音乐类型
+            || (@$_FILES["file"]["type"] == "audio/x-wav") || (@$_FILES["file"]["type"] == "audio/x-pn-realaudio")
+            &&(@$_FILES["file"]["size"] <500 * 1024 * 1024)){
+            $files['filetype']=3;
+            createFolder("upload/".$files['userId']."/music/");
+            $files['path']="";
+        }
+        else{
+            $files['filetype']=5;
+            createFolder("upload/".$files['userId']."/other/");
+        }
+        db('files')->insert($files);
+
+
     }
     public function search(){       //查找功能
         $data=$_GET('search');
@@ -114,13 +115,44 @@ class Index extends Controller
 
     public function remove(){           //移动
         $parentid=$_POST['parentid'];
+        $folderid=$_POST['folderid'];
+        $type=$_POST['type'];
+        if($type=='file'){
+            db('files')->where('fileid',$folderid)->update('folderid',$parentid);
+        }
+        else{
+            db('folder')->where('folderid',$folderid)->update('parentid',$parentid);
+        }
     }
     public function delete(){           //删除
+        $add_data['createtime']=date(date('Y-m-d H:i:s'));
+        $add_data['fid']=$_POST['id'];
+        $add_data['type']=$_POST['type'];
+        if($_POST['type']=='file'){
+            $fileid=$_POST['id'];
+            db('files')->where('fileid',$fileid)->update('is_recycle',1);
+            db('bin')->insert($add_data);
+        }
+        else{
+            $folderid=$_POST['id'];
+            db('folder')->where('folderid',$folderid)->update('is_recycle',1);
+            db('bin')->insert($add_data);
+        }
 
-        $id=$_POST['foldername'];
 
     }
-    public function download(){         //下载
+    public function download(){ //下载
+        $fileid=$_GET('fileid');
+        $list=db('files')->where('fileid',$fileid)->select();
+//        import('ORG.Net.Http');
+//        $http=new \ORG\Net\Http();
+        $filename=$list['path'];
+        $basename=$list['filename'];
+        header("Content-Type:image/png");
+        header("Content-Disposition:attachment;filename=".$basename['basename']);
+        header("Content-Length:".$list['filesize']);
+        readfile($filename);
+
 
 
     }
@@ -129,8 +161,9 @@ class Index extends Controller
         $where['userId']=Session::get('uinfo')['userId'];
         $where['parentid']=$path;
         $where['is_recycle']=0;
-        $list=db('folder')->where($where)->select();
-        echo json_encode($list);
+        $listfloder=db('folder')->where($where)->select();
+        echo json_encode($listfloder);
+        $listfile=db('files')->where($where)->select();
     }
     public function Photos(){   // 查找图片类型的文件
         $where['userId']=Session::get('uinfo')['userId'];
@@ -169,7 +202,7 @@ class Index extends Controller
         echo json_encode($list);
     }
     public function recycleBin(){     //回收站
-        $list1=db('folder')->where(['userId'=>Session::get('uinfo')['userId'],'is_recycle'=>1])->select();
+
 
 
     }
